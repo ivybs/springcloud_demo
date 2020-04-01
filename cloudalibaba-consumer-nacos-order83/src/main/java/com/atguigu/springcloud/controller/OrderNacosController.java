4e134a52774d434b2d89657a0e71ac3ca84e3843
ivybs <840464844@qq.com>
2020-04-01 20:34:05 +0800

    更新至nacos服务注册的消费者和提供者

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/controller/OrderNacosController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderNacosController {
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-user-service}")
+    private String SERVICE_URL;
+
+    @GetMapping("/consumer/payment/nacos/{id}")
+    public String get(@PathVariable("id")Integer id){
+        return restTemplate.getForObject(SERVICE_URL+"/nacos/payment/" + id,String.class);
+    }
+
+}
