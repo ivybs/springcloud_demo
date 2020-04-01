1af95f9c02977cc773a26660b2cdfc8feeba4ad6
ivybs <840464844@qq.com>
2020-04-01 17:00:03 +0800

    更新至nacos服务配置中心

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@Slf4j
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        log.info("*****消费者1号，收到的消息： " + message.getPayload() + "\t" + serverPort);
+
+    }
+}
